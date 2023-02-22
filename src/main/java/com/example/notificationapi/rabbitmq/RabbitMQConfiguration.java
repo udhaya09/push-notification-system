fc95f77dacd35ed6e9abf17b3ae00366b284ec92
OPTISOLBUSINESS\udhayaprakash <udhayaprakash@optisolbusiness.com>
2023-02-22 10:56:59 +0530

    comments added

--- src/main/java/com/example/notificationapi/rabbitmq/RabbitMQConfiguration.java
+++ src/main/java/com/example/notificationapi/rabbitmq/RabbitMQConfiguration.java
@@ -14,33 +14,38 @@ public class RabbitMQConfiguration {
 
 	@Value("${rabbitmq.queue.name}")
 	public static final String QUEUE = "pn_poc_new_product_queue";
-	
+
 	@Value("${rabbitmq.exchange.name}")
 	public static final String EXCHANGE = "pn_poc_new_product_exchange";
-	
+
 	@Value("${rabbitmq.routingkey.name}")
 	public static final String ROUTING_KEY = "pn_poc_new_product_routing_key";
 
+	// Defines the RabbitMQ queue
 	@Bean
 	public Queue queue() {
 		return new Queue(QUEUE);
 	}
 
+	// Defines the RabbitMQ exchange
 	@Bean
 	public TopicExchange exchange() {
 		return new TopicExchange(EXCHANGE);
 	}
 
+	// Defines the binding between the queue and the exchange
 	@Bean
 	public Binding binding(Queue queue, TopicExchange exchange) {
 		return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
 	}
 
+	// Defines the message converter to be used for message serialization
 	@Bean
 	public MessageConverter converter() {
 		return new Jackson2JsonMessageConverter();
 	}
 
+	// Defines the RabbitMQ template to be used for sending messages
 	@Bean
 	public AmqpTemplate template(ConnectionFactory connectionFactory) {
 		final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
